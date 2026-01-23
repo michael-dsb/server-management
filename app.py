1b686215275e3852aa6bf609d5b5dc51752b7c5e
michael-dsb <michaeldemidov8@gmail.com>
2026-01-23 00:22:37 -0500

    added server config, dashboard

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template
 # Create the Flask application
 app = Flask(__name__)
 
-# Home page route
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
+
+# about page route
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 # Run the app
 if __name__ == "__main__":
